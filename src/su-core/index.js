fd1c77999d4b875505c9d8d5c802bf522c448f4e
Ashish Prasher <prasher.ashish@gmail.com>
2025-05-02 12:56:49 +0530

    uncommented search tool

--- src/su-core/index.js
+++ src/su-core/index.js
@@ -3,6 +3,6 @@ import { initializeAnalyticsTools } from './su-core-analytics.js';
 
 export const initializeSuCoreTools = async ({ server, creds }) => {
   console.error("Initializing suCore tools...");
-  // await initializeSearchTools({ server, creds });
+  await initializeSearchTools({ server, creds });
   await initializeAnalyticsTools({ server, creds });
 };
